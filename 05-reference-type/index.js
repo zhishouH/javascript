4938d6523fa0887a70ef36c625655c3dfdc10488
zhishouhuang <zhishouhuang@gmail.com>
2020-07-17 22:57:32 +0800

    js

---
+++ 05-reference-type/index.js
@@ -0,0 +1,84 @@
+// object类型
+// new + Object构造函数
+// var person = new Object()
+// person.name = 'zhishouh'
+// person.age = 19
+// console.log(person.name + ' ' + person.age)
+
+// 对象字面量表示法
+// var person = {
+//   name: 'zhishouh',
+//   age: 19
+// }
+// console.log(person.name + ' ' + person.age)
+
+// function displayInfo (args) {
+//   var output = ''
+
+//   if (typeof args.name === 'string') {
+//     output += 'Name: ' + args.name + '\n'
+//   }
+//   if (typeof args.age === 'number') {
+//     output += 'Age: ' + args.age + '\n'
+//   }
+//   console.log(output)
+// }
+// displayInfo({
+//   name: 'zhishouh',
+//   age: 19
+// })
+// displayInfo({
+//   name: 'pepsi'
+// })
+
+// Array类型
+// var colors = new Array()
+// var colors = new Array(20)
+// var colors = new Array('red', 'blue', 'green')
+// var colors = new Array(3) // 创建一个包含3项的数组
+// var colors = new Array('Greg') // 创建一个包含1项，即字符串"Greg"的数组
+
+// var colors = ['red', 'blue', 'green'] // 创建一个包含3个字符串的数组
+// console.log(colors[0])
+// var names = [] // 创建一个空数组
+// console.log(names[0])
+
+// var colors = ['red', 'blue', 'green'] // 定义一个字符串数组
+// console.log(colors[0]) // 显示第一项
+// colors[1] = 'black' // 修改第二项
+// colors[3] = 'brown' // 新增第四项
+
+// 检测数组
+// var colors = ['red', 'blue', 'green']
+// if (Array.isArray(colors)) {
+//   for (var i = 0; i < colors.length; i++) {
+//     console.log(colors[i])
+//   }
+// }
+
+// 转换方法
+// var colors = ['red', 'blue', 'green']
+// console.log(colors.toString()) // red,blue,green
+// console.log(colors.valueOf()) // red,blue,green
+// console.log(colors) // red,blue,green
+
+var person1 = {
+  toLocaleString: function () {
+    return 'zhihsouh'
+  },
+  toString: function () {
+    return 'pepsi'
+  }
+}
+var person2 = {
+  toLocaleString: function () {
+    return 'shabi'
+  },
+  toString: function () {
+    return 'hanhan'
+  }
+}
+var people = [person1, person2]
+console.log(people)
+console.log(people.toString())
+console.log(people.toLocaleString())
