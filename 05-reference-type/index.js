1677f59efee75dfb4f80b6853ed54cc0bfe5b417
zhishouhuang <zhishouhuang@gmail.com>
2020-07-22 20:27:32 +0800

    引用类型

--- 05-reference-type/index.js
+++ 05-reference-type/index.js
@@ -132,6 +132,23 @@
 // console.log(colors.length) // 2
 
 // 重排序方法
-var values = [1, 2, 3, 4, 5]
-values.reverse()
+// var values = [1, 2, 3, 4, 5]
+// values.reverse()
+// console.log(values) // 5，4，3，2，1
+
+// var values = [0, 1, 5, 10, 15]
+// values.sort()
+// console.log(values) // 0,1,10,15,5
+
+function compare (value1, value2) {
+  if (value1 < value2) {
+    return -1
+  } else if (value1 > value2) {
+    return 1
+  } else {
+    return 0
+  }
+}
+var values = [0, 1, 5, 10, 15]
+values.sort(compare)
 console.log(values)
