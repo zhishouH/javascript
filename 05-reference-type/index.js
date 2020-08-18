19fc597f1c77729b384e75f0f8bb84ef61dae733
zhishouhuang <zhishouhuang@gmail.com>
2020-08-18 15:11:38 +0800

    Update index.js

--- 05-reference-type/index.js
+++ 05-reference-type/index.js
@@ -390,10 +390,10 @@ var text = 'this has been a short summer'
 var pattern = /(.)hort/g
 
 if (pattern.test(text)) {
-  console.log(RegExp.input)
-  console.log(RegExp.leftContext)
-  console.log(RegExp.rightContext)
-  console.log(RegExp.lastMatch)
-  console.log(RegExp.lastParen)
-  console.log(RegExp.multiline)
+  // console.log(RegExp.input)
+  // console.log(RegExp.leftContext)
+  // console.log(RegExp.rightContext)
+  // console.log(RegExp.lastMatch)
+  // console.log(RegExp.lastParen)
+  console.log(pattern.multiline)
 }
