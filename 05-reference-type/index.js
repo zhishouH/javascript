8a9c35c295ef7be9466ce9ef1c8dfb3dcf0277e5
zhishouhuang <zhishouhuang@gmail.com>
2020-08-31 19:44:24 +0800

    Update index.js

--- 05-reference-type/index.js
+++ 05-reference-type/index.js
@@ -552,5 +552,5 @@ function callSum1 (num1, num2) {
 function callSum2 (num1, num2) {
   return sum.apply(this, [num1, num2])
 }
-console.log(callSum1(10, 10))
-console.log(callSum2(10, 10))
+console.log(callSum1(10, 10)) // 20
+console.log(callSum2(10, 10)) // 20
