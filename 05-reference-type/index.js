2ae7c3d685fb8eff6c66d4d11a1893e87bef9a89
zhishouhuang <zhishouhuang@gmail.com>
2020-08-31 19:43:46 +0800

    Update index.js

--- 05-reference-type/index.js
+++ 05-reference-type/index.js
@@ -529,15 +529,28 @@
 
 // 函数属性和方法
 // length
-function sayName (name) {
-  console.log(name)
-}
+// function sayName (name) {
+//   console.log(name)
+// }
+// function sum (num1, num2) {
+//   return num1 + num2
+// }
+// function sayHi () {
+//   console.log('Hi')
+// }
+// console.log(sayName.length)
+// console.log(sum.length)
+// console.log(sayHi.length)
+
+// apply
 function sum (num1, num2) {
   return num1 + num2
 }
-function sayHi () {
-  console.log('Hi')
+function callSum1 (num1, num2) {
+  return sum.apply(this, arguments)
 }
-console.log(sayName.length)
-console.log(sum.length)
-console.log(sayHi.length)
+function callSum2 (num1, num2) {
+  return sum.apply(this, [num1, num2])
+}
+console.log(callSum1(10, 10))
+console.log(callSum2(10, 10))
