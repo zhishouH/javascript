74cef83fb920116ed9d8e8a78b031493dca3d7af
zhishouhuang <zhishouhuang@gmail.com>
2020-09-02 20:05:46 +0800

    Update index.js

--- 05-reference-type/index.js
+++ 05-reference-type/index.js
@@ -555,10 +555,22 @@
 // console.log(callSum1(10, 10)) // 20
 // console.log(callSum2(10, 10)) // 20
 
-function sum (num1, num2) {
-  return num1 + num2
+// function sum (num1, num2) {
+//   return num1 + num2
+// }
+// function callSum (num1, num2) {
+//   return sum.call(this, num1, num2)
+// }
+// console.log(callSum(10, 10))
+
+window.color = 'red'
+var o = { color: 'blue' }
+
+function sayColor () {
+  console.log(this.color)
 }
-function callSum (num1, num2) {
-  return sum.call(this, num1, num2)
-}
-console.log(callSum(10, 10))
+sayColor()
+
+sayColor.call(this)
+sayColor.call(window)
+sayColor.call(o)
